5813a1d5bf5d9ca8616f486b3a0b9ee209240330
mangompae <mgomezpaez280705@gmail.com>
2024-10-16 09:32:03 +0200

    Se ha cambiado el color incorrecto

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
-class HolaMundo{
+public class HolaMundo{
     public static void main(String[] args) {
-        String rojo="\033[32m";
+        String rojo="\033[31m" ;
         String azul="\033[34m";
         System.out.println("Hoal mundi");
 }
