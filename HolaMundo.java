f5f396b279e8f2481f74d674af3060982da4f734
mangompae <mgomezpaez280705@gmail.com>
2024-10-16 09:18:47 +0200

    versión inicial2

--- HolaMundo.java
+++
@@ -1,6 +0,0 @@
- public class HolaMundo{
-    public static void main(String[] args) {
-        String rojo="\033[33m";
-        String azul="\033[35m";
-        System.out.println("Hoal mundi");
-}
